08f4c7f2ad6e56e6567e25ed7f9d7faa3421e7df
EdwinE97 <94918451+EdwinE97@users.noreply.github.com>
2022-04-06 14:56:15 -0400

    made footer and installed fontawesome

--- netflix/src/App.js
+++ netflix/src/App.js
@@ -2,13 +2,20 @@ import Movies from './Movies';
 import './App.css';
 import Contact from './contact'
 import './contact.css'
+import Footer from './components/Footer'
+
 
 function App() {
   return (
     <div className="App">
   
         <Movies/>
+
     <Contact/>
+
+    <Footer/>
+    
+     
     </div>
   );
 }
