6bf42f5caad1e7572cfcd5bf64413b57406c6809
alonramz <alondraramirezh@gmail.com>
2022-04-06 16:13:14 -0400

    recent change of hover

--- netflix/src/Movies.js
+++ netflix/src/Movies.js
@@ -29,7 +29,10 @@ function Movies() {
             Netflix Originals
         </h2>
         <div className='row_posters'>
-        <img className='row_poster large_row' src='/moviesimages/howtotraindragon.jpg' alt='dragon'/>
+            {/* <div className='image_content'> */}
+                <img className='row_poster large_row' src='/moviesimages/howtotraindragon.jpg' alt='dragon'/>
+                {/* THIS IS MY FAVORITE MOVIE AND SERIES OF ALL TIME! I never get bored of it, the strong messages and emotional moments between characters makes it even more loveable.
+            </div> */}
             <img className='row_poster large_row' src='/moviesimages/hometeam.jpg' alt='hometeam'/>
             <img className='row_poster large_row' src='/moviesimages/sorrytobotheryou.png' alt='sorrytobotheryou'/>
             <img className='row_poster large_row' src='/moviesimages/shrek.png' alt='shrek'/>
