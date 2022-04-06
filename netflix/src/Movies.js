ee53974394c3a152cd406fcac6935adae5c7903d
alonramz <alondraramirezh@gmail.com>
2022-04-06 11:39:49 -0400

    added row and changes some images

--- netflix/src/Movies.js
+++ netflix/src/Movies.js
@@ -33,7 +33,7 @@ function Movies() {
             <img className='row_poster large_row' src='/moviesimages/hometeam.jpg' alt='hometeam'/>
             <img className='row_poster large_row' src='/moviesimages/sorrytobotheryou.png' alt='sorrytobotheryou'/>
             <img className='row_poster large_row' src='/moviesimages/shrek.png' alt='shrek'/>
-            <img className='row_poster large_row' src='/moviesimages/thebubble.png' alt='thebubble'/>
+            <img className='row_poster large_row' src='/moviesimages/thebubble.jpg' alt='thebubble'/>
             <img className='row_poster large_row' src='/moviesimages/fourbrothers.jpeg' alt='fourbrothers'/>
             <img className='row_poster large_row' src='/moviesimages/therental.jpg' alt='therental'/>
             <img className='row_poster large_row' src='/moviesimages/blow.png' alt='blow'/>
@@ -59,23 +59,23 @@ function Movies() {
         </div>   
     </div>
 
-    {/* top rated now */}
-    {/* <div className='movie_row'>
+    {/* coming soon */}
+    <div className='movie_row'>
         <h2>
-            Trending Now
+            Coming Soon
         </h2>
         <div className='row_posters'>
-            <img className='row_poster' src='/moviesimages/trustno1.png' alt='trustnoone'/>
-            <img className='row_poster' src='/moviesimages/inception.png' alt='inception'/>
-            <img className='row_poster' src='/moviesimages/dontlookip.png' alt='dontlookup'/>
-            <img className='row_poster' src='/moviesimages/uncutgems.png.jpeg' alt='uncutgems'/>
-            <img className='row_poster' src='/moviesimages/cloudy.png' alt='cloudywithchanceofmeatballs'/>
-            <img className='row_poster' src='/moviesimages/rush.jpg' alt='rush'/>
-            <img className='row_poster' src='/moviesimages/captainnova.jpg' alt='captainnova'/>
-            <img className='row_poster' src='/moviesimages/21bridges.jpg' alt='21bridges'/>
-            <img className='row_poster' src='/moviesimages/survivingparadise.jpg' alt='survivingparadise'/>
-        </div>   
-    </div> */}
+        <img className='row_poster large_row' src='/moviesimages/whitehot.png' alt='whitehot'/>
+            <img className='row_poster large_row' src='/moviesimages/chooseordie.png' alt='chooseordie'/>
+            <img className='row_poster large_row' src='/moviesimages/heirsland.png' alt='heirsland'/>
+            <img className='row_poster large_row' src='/moviesimages/onepiece.jpg' alt='onepiece'/>
+            <img className='row_poster large_row' src='/moviesimages/themanofgod.png' alt='themanofgod'/>
+            <img className='row_poster large_row' src='/moviesimages/thelegomovie.png' alt='lego'/>
+            <img className='row_poster large_row' src='/moviesimages/thegrayman.png' alt='grayman'/>
+            <img className='row_poster large_row' src='/moviesimages/bossbaby.jpg' alt='bossbaby'/>
+            <img className='row_poster large_row' src='/moviesimages/silvertonsiege.jpg' alt='adamproject'/>
+        </div>
+    </div>
     </>
   )
 }
