1f8ef7b36079c934fb7342ab6c5f5e33aa720da1
EdwinE97 <94918451+EdwinE97@users.noreply.github.com>
2022-04-08 10:06:53 -0400

    Updated contact w validation

--- netflix/src/contact.js
+++ netflix/src/contact.js
@@ -1,31 +1,46 @@
 import './App.css';
 import './contact.css'
 import {Link} from 'react-router-dom';
+import Nav from './components/Nav'
 
+  
 
-
-const Contact = () =>{
-
+ const Contact = () => {
+  
     return(
+        
         <>
+        
+        <Nav />
         <div>
         <Link to="/"><img src="accountimages/Netflix_2015_logo.svg"  alt="logo" class="logo"/></Link>
         </div>
+        <div className='contact-container'>
+        <header className='head'>
+            <div className='head-contact'>
+                <h1 className='css-contact-us'>Contact Us</h1>
+            </div>
+        </header>
 
-        <form  className = 'form-body'>
+        <form  method = 'POST' className = 'form-body' action="/api/contact" >
             <fieldset className='input-container'>
             <div className = 'form-input-container'>
-        <input type = 'text' placeholder = ' First Name'/>
-        <input type = 'text' placeholder = 'Last Name'/> 
+        <input name="first name" placeholder = ' First Name' required/>
+        <input  name="last name" placeholder = 'Last Name' required/>
+      
         </div>
         <div className = 'form-input-container'>
-        <input type = 'text' placeholder = 'Email Address'/>
-        <input type = 'text' placeholder = 'Phone'/> 
-      
+        <input type = 'text' name="email" placeholder='Email'required/>
+        <input type = '' name="phone" placeholder = 'Phone'  required/> 
+       
+       
+
+          
         </div>
         </fieldset>
         <button type= 'submit' className='default button'><span className='send'>Send</span></button>
         </form>
+        </div>
         
         </>
     )
