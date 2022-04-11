992e5aeec1ccc56f0c81a05c8d09f9a3f561affd
Dood3l <bk020220@gmail.com>
2022-04-11 17:19:38 -0400

    fixed ipad responsiveness

--- netflix/src/contact.js
+++ netflix/src/contact.js
@@ -2,7 +2,6 @@ import './App.css';
 import {React, useState,} from 'react';
 import './contact.css'
 import {Link} from 'react-router-dom';
-import Nav from './components/Nav'
 import axios from 'axios'
 
 
